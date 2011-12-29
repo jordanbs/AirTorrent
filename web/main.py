40834c833b1ee3ffb0abb491e6b0eec3f4b9eb44
Caleb Hearon <caleb@chearon.net>
2011-12-28 22:17:43 -0500

    First pass implementing templates

--- web/main.py
+++ web/main.py
@@ -1,69 +1,23 @@
 import cherrypy
 import os
 import sqlite3
-
+import mako
 
 class AirTorrent:
-
+    def __init__(self):
+        mako_lookup = TemplateLookup(directories=['.'])
+        self.main_tpl = Template(filename='/main.tpl',
+                                 module_directory='./templates',
+                                 lookup=mako_lookup)
 
     def index(self):
-        # Ask for the user's name.
-        value = '''
-<html>
-<head>
-<link href="static/css/stylesheet.css" type="text/css" rel="stylesheet" />  
-<link href="static/css/table.css" type="text/css" rel="stylesheet" />
-<link href="static/css/upload.css" type="text/css" rel="stylesheet" />
-<script type="text/javascript" src="static/scripts/enhance.js"></script>        
-<script type="text/javascript" src="static/scripts/complete.min.js"></script>
-<script type="text/javascript" language="javascript" src="static/scripts/jquery.js"></script>
-<script type="text/javascript" language="javascript" src="static/scripts/jquery.event.drag.js"></script>
-<script type="text/javascript" language="javascript" src="static/scripts/jquery.dataTables.js"></script>
-<script type="text/javascript" language="javascript" src="static/scripts/ColReorder.js"></script>
-<script type="text/javascript" language="javascript" src="static/scripts/AirTorrentWebUI.js"></script>
-<script type="text/javascript" src="static/scripts/jQuery.fileinput.js"></script>
-</head>
-<body>
-'''
-        value+=self.generateBody()
-        return value;
+        stuff = {
+            audiolink: 'libraryDiv?libType=audio',
+            videolink: 'libraryDiv?libType=video',
+            torrentlink: 'upload?temp=0'
+        }
+        return self.main_tpl.render(stuff)
     index.exposed = True
-
-    
-    def generateBody(self):
-        #set up the menu div with proper playlists and filters
-        #put something in the right div
-        audiolink = 'libraryDiv?libType=audio'
-        videolink = 'libraryDiv?libType=video'
-        torrentlink = 'upload?temp=0'
-        audio='<div data-ajaxlink="'+audiolink+'" class="menuLinkDiv library notloaded" id="menuLinkDivAudio"><div class="menuIcon" id="iconAudioDiv"></div><div class="menuTextLink">Audio</div></div>\n'
-        video='<div data-ajaxlink="'+videolink+'" data-libid="0" class="menuLinkDiv library selected loaded" id="menuLinkDivVideo"><div class="menuIcon" id="iconVideoDiv"></div><div class="menuTextLink">Video</div></div>\n'
-        torrent='<div data-ajaxlink="'+torrentlink+'" class="menuLinkDiv torrent notloaded" id="menuLinkDivTorrent"><div class="menuIcon" id="iconTorrentDiv"></div><div class="menuTextLink">Torrent</div></div>\n'
-        value = '<div id="menuDiv">'+video+audio+torrent+'</div>\n'
-        value+='''
-<div id="contentDiv"></div>
-
-<div id="controlsDiv">
-<div id=controlButtons><div class="skipPrvBtnImgDiv"></div><div class="stopBtnImgDiv"></div><div class="playBtnImgDiv play enabled"></div><div class="skipFwdBtnImgDiv"></div></div>
-<div class="progressTimeline"><div class="progressTimelineHeader"><div class="progressTimelineTitle"><img class="progressTimelineIconAudio" src="static/images/mini-icon-audio.png"><img class="progressTimelineIconVideo" src="static/images/mini-icon-video.png"></div><div class="progressTimelineAlbum"></div><div class="progressTimelineArtist"></div></div><div class="progressTimelineLeftDiv"></div><div class="progressTimelineCenterDiv"></div><div class="progressTimelineRightDiv"></div><div class="progressTimelineTimePlayed">0:00</div><div class="progressTimelineTimeLeft">-0:00</div><div class="playbackTrack"><div class="playbackCursor"></div><div class="playbackTrackLeft"></div><div class="playbackTrackCenter"></div><div class="playbackTrackRight"></div><div class="playbackTrackLeftProgress"></div><div class="playbackTrackCenterProgress"></div><div class="playbackTrackRightProgress"></div></div></div>
-<div class="volumeDiv"><div class="volumeIconDiv"></div><div class="volumeTrackDiv"><div class="volumeKnobDiv"></div></div></div>
-<div class="shuffleLoopDiv"><div class="shuffleBtnDiv off"></div><div class="loopBtnDiv off"></div></div>
-</div>
-
-<div id="mediaDiv">
-<div id="videoDiv">
-</div>
-<div id="audioDiv">
-<audio class="mediaPlayer audio" width="0%" height="0%"><source src="static/Kalimba.mp3"></audio>
-</div>
-</div>
-
-</body>
-</html>
-'''
-        return value
-    generateBody.exposed = True
-    
     
     def mediaDiv(self, source = None): 
         value = '<div id="media"><div id="mediaTitle">'+'Title as supplied by some AJAX goes here'+'</div>'
