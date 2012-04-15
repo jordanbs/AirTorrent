b9dd0226c74ac801cc8603247c8f5ced7c77c982
Jordan Schneider <jbschne@umich.edu>
2012-04-15 06:50:16 +0000

    Convert hls_server to be RESTful, rid hardcoded constants

--- server/hls_server.py
+++ server/hls_server.py
@@ -5,13 +5,11 @@ import tempfile
 from torrent_session_manager import TorrentSessionManager
 from miro_library_manager import MiroLibraryManager
 
-#TODO: move this to a config
-broadcast_address = 'demo.airtorrent.tk'
-
 class HLSTorrentSession(object):
 
-    def __init__(self, media_path):
+    def __init__(self, media_path, broadcast_address):
         self.media_path = media_path
+        self.broadcast_address = broadcast_address
         self._setup()
 
     def _setup(self):
@@ -21,7 +19,6 @@ class HLSTorrentSession(object):
 
         self.miro_library_manager = MiroLibraryManager(self.segment_request_path_func_library)
 
-        self.broadcast_address = broadcast_address
 
     def add_torrent(self, torrent_file_path):
         # returns a dict of files to choose from
@@ -43,20 +40,14 @@ class HLSTorrentSession(object):
         return self.miro_library_manager.get_chunk(itemid, chunk)
 
     def segment_request_path_func(self, torrent_info_hash, enclosure):
-        address, addrlength = self.socket.getsockname()
-        listen_address, port = self.server_address
         # TODO: fix TranscodeObject.get_chunk() to append urls properly to get rid of junk
-        # FIXME: Don't hard code this...
-        address = "demo.airtorrent.tk"
-        return ('http://%s:%d/segments/%s.%s?junk=0' % (address, port, torrent_info_hash, enclosure))
+        address = self.broadcast_address
+        return ('http://%s/segments/%s.%s?junk=0' % (address, torrent_info_hash, enclosure))
 
     def segment_request_path_func_library(self, itemid, enclosure):
-        address, addrlength = self.socket.getsockname()
-        listen_address, port = self.server_address
         # TODO: fix TranscodeObject.get_chunk() to append urls properly to get rid of junk
-        # FIXME: Don't hard code this...
-        address = "demo.airtorrent.tk"
-        return ('http://%s:%d/segments/library/%d.%s?junk=0' % (address, port, itemid, enclosure))
+        address = self.broadcast_address
+        return ('http://%s/segments/library/%d.%s?junk=0' % (address, itemid, enclosure))
 
     def get_chunk(self, torrent_info_hash, chunk):
         return self.torrent_session_manager.get_chunk(torrent_info_hash, chunk)
@@ -64,54 +55,102 @@ class HLSTorrentSession(object):
     def shutdown(self):
         self.torrent_session_manager.stop()
 
-class HLSPlaylist(object):
-    def __init__(self, hls_session):
-        self.hls_session = hls_session
+class Playlist(object):
+    def __init__(self, hls_torrent_session):
+        self.hls_torrent_session = hls_torrent_session
 
-    def default(self, infohash):
+    exposed = True
+
+    def GET(self, infohash):
+        import ipdb; ipdb.set_trace()
         ext = '.m3u8'
         if infohash.endswith(ext):
             infohash = infohash[:-len(ext)]
 
-        playlist = self.hls_session.get_playlist(infohash)
+        playlist = self.hls_torrent_session.get_playlist(infohash)
         if not playlist:
             raise cherrypy.HTTPError(404)
 
         return playlist
 
-    default.exposed = True
-
-class HLSSegments(object):
+class Segment(object):
     
-    def __init__(self, hls_session):
-        self.hls_session = hls_session
+    def __init__(self, hls_torrent_session):
+        self.hls_torrent_session = hls_torrent_session
 
-    def default(self, infohash, junk, chunk):
+    exposed = True
+
+    def GET(self, infohash, junk, chunk):
         chunk = int(chunk)
-        segment_file = self.hls_session.get_chunk(infohash, chunk)
+        segment_file = self.hls_torrent_session.get_chunk(infohash, chunk)
         if not segment_file:
             raise cherrypy.HTTPError(404)
         #TODO: dispatcher?
         return segment_file
 
-    default.exposed = True
+class Library(object):
 
-class HLSLibrary(object):
-
-    def __init__(self, hls_session):
-        self.hls_session = hls_session
-        self.playlist = HLSPlaylist(hls_session)
-        self.segments = HLSSegments(hls_session)
+    def __init__(self, hls_torrent_session):
+        self.hls_torrent_session = hls_torrent_session
+        self.playlist = Playlist(hls_torrent_session)
+        self.segments = Segment(hls_torrent_session)
         self.playlist.exposed = True
         self.segments.exposed = True
+
+class Torrent(object):
+    def __init__(self, hls_torrent_session):
+        self.hls_torrent_session = hls_torrent_session
+
+    exposed = True
+
+    def POST(self, torrent_file):
+        out = """<html>
+        <body>
+            torrent_file filename: %s<br />
+            torrent_file mime-type: %s<br />
+        </body>
+        </html>"""
+
+        data = torrent_file.file.read()
+    
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(data)
+        temp_file.close()
+
+        self.hls_torrent_session.add_torrent(temp_file.name)
+        return out % (temp_file.name, torrent_file.content_type)
+
 
 class HLSTorrentServer(object):
 
-    def __init__(self, media_path):
-        self.hls_torrent_session = HLSTorrentSession(media_path)
+    def __init__(self, media_path, broadcast_address):
+        self.hls_torrent_session = HLSTorrentSession(media_path, broadcast_address)
         
-        self.playlist = HLSPlaylist(self.hls_torrent_session)
-        self.segments = HLSSegments(self.hls_torrent_session)
+        self.playlist = Playlist(self.hls_torrent_session)
+        self.segments = Segment(self.hls_torrent_session)
+        self.torrent = Torrent(self.hls_torrent_session) 
         self.playlist.exposed = True
         self.segments.exposed = True
 
+    exposed = True
+
+    def GET(self):
+        return """
+            <html><body>
+                <h2>Upload a file</h2>
+                    <form action="torrent" method="post" enctype="multipart/form-data">
+                        filename: <input type="file" name="torrent_file" /><br />
+                        <input type="submit" />
+                    </form>
+                <h2>Download a file</h2>
+                <a href='download'>This one</a>
+            </body></html>
+            """
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    config = 'config.ini'
+    cherrypy.config.update(config)
+    hls_torrent_server = HLSTorrentServer(cherrypy.config['torrent_save_dir'],
+                                          cherrypy.config['broadcast_addr'])
+    cherrypy.quickstart(hls_torrent_server, '/', config)
