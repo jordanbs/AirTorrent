ca32bcff033684cb5f45704eeea5481e012fea80
Caleb Hearon <caleb@chearon.net>
2012-04-18 03:17:26 -0400

    Add initial HTML, CSS and JS\!

---
+++ client/js/at-base.js
@@ -0,0 +1,12 @@
+/**
+ * File: at-base.js
+ * Purpose: Establish AT namespace and provide low level utility functions and
+ * constants for AirTorrent.  jQuery plugins go here along with any other
+ * library customizations, such as Backbone hacks.
+ */
+
+/**
+ * Root namespace for AirTorrent.  Establish the new object or use existing
+ */
+AT = AT || {};
+
---
+++ client/js/at-player.js
@@ -0,0 +1,8 @@
+/**
+ * File: at-player.js
+ * Purpose: Provides the player object which takes a torrent as input and allows
+ * the user to control playback.
+ */
+
+AT.Player = {};
+
